92f9a320d40afb829975a7378de5d171b2e18ed1
TheGhossst <nandukrishnapkm97@gmail.com>
2024-10-29 13:44:41 +0530

    add lex program to count comments

---
+++ extraqustions/lex-count-comments/input.c
@@ -0,0 +1,10 @@
+// Input file example.c
+#include <stdio.h>
+
+/* This is a multi-line comment
+   spanning multiple lines */
+int main() {
+    // This is a single-line comment
+    printf("Hello // This is not a comment");
+    return 0; /* End of program */
+}
